78fa73c6a071a611de7712c845c28c3efaee999c
levievans <levisevans@gmail.com>
2024-07-24 17:36:01 +0100

    implement simple web server on the EC2 instance that can HTTP requests

--- main.tf
+++ main.tf
@@ -10,9 +10,30 @@ aws provider.  */
 resource "aws_instance" "example" {
   ami           =  "ami-07c8c1b18ca66bb07" # Ubuntui 20.04
   instance_type = "t3.micro"               # free tier [https://aws.amazon.com/ec2/instance-types/t3/]
+  vpc_security_group_ids = [aws_security_group.instance.id] # we need to tell the instance to use the security group we created!
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  user_data_replace_on_change = true
 
   tags = {
     Name = "terraform-tutorial"
+  }
+}
+
+/*By default, AWS does not allow any incoming or outgoing traffic 
+from an EC2 instance, so we need to setup it up to allow traffic 
+on port 8080 by creating a security group*/
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # allow TCP requests from any IP address
   }
 }
 
